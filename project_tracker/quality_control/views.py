6d706f3e68f3f59e3e11fcb96411b50bb089b638
Evsey Kirichkov <evseykirichkov@MacBook-Pro-Evsey.local>
2024-04-29 18:46:45 +0300

    URLs for quality_control

--- project_tracker/quality_control/views.py
+++ project_tracker/quality_control/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Система контроля качества<br><a href='/bugs/'>Список всех багов</a><br><a href='/features/'>Запросы на улучшение</a>")
+
+def bug_list(request):
+    return HttpResponse("Cписок отчетов об ошибках")
+
+def feature_list(request):
+    return HttpResponse("Список запросов на улучшение")
+
+def bug_detail(request, bug_id):
+    return HttpResponse(f"Детали бага {bug_id}")
+
+def feature_detail(request, feature_id):
+    return HttpResponse(f"Детали улучшения {feature_id}")
